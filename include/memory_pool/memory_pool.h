c8f938b29ec0054332f66f1815ca91943863025a
Aleksandar Dakic <aleksandardakic16@gmail.com>
2021-04-03 16:17:50 +0200

    Add memory_pool constructor

--- include/memory_pool/memory_pool.h
+++ include/memory_pool/memory_pool.h
@@ -3,30 +3,27 @@
 #include <tuple>
 #include <array>
 #include "memory_chunk.h"
+#include "memory_common_types.h"
 #include <iostream>
-
-struct memory_config
-{
-    std::size_t chunk_size;
-    std::size_t chunk_count;
-};
 
 class memory_pool
 {
 public:
-    template <typename... Configs, std::enable_if_t<std::is_same_v<Configs..., memory_config>>>
+    template <typename... Configs>
     memory_pool(const Configs... configs)
     {
-        std::tuple<Configs...> mem_config(configs...);
-        std::apply([this](memory_config& conf)
-                    {std::cout << conf.chunk_size <<" " << conf.chunk_count << "\n";}, 
-                    mem_config);
+        if (sizeof...(configs) > m_max_chunk_number)
+        {
+            throw std::bad_alloc();
+        }
 
+        std::size_t i = 0;
+        ((m_chunks[i++] = {configs.chunk_size, configs.chunk_size}), ...);
     }
 
     [[nodiscard]] std::uint8_t* allocate(std::size_t node_size, std::size_t count);
     void deallocate(std::uint8_t* ptr, std::size_t count);
 private:
-    static constexpr uint16_t m_max_chunk_number {100};
+    static constexpr uint16_t m_max_chunk_number {5};
     std::array<memory_chunk, m_max_chunk_number> m_chunks;
 };
